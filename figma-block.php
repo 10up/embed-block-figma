0a7f3aedb04bdd530f7f496fa035f8fd31d2668c
Darin Kotter <darin.kotter@gmail.com>
2024-03-11 15:39:46 -0600

    Add PHP version checking. Remove things that aren't needed

--- figma-block.php
+++ figma-block.php
@@ -14,7 +14,7 @@
 
 namespace FigmaBlock;
 
-use TenUpToolkit;
+use function TenUpToolkit\set_dist_url_path;
 
 // Useful global constants.
 define( 'FIGMA_BLOCK_VERSION', '1.1.0' );
@@ -23,6 +23,49 @@ define( 'FIGMA_BLOCK_PATH', plugin_dir_path( __FILE__ ) );
 define( 'FIGMA_BLOCK_INC', FIGMA_BLOCK_PATH . 'includes/' );
 define( 'FIGMA_BLOCK_DIST_URL', FIGMA_BLOCK_URL . 'dist/' );
 define( 'FIGMA_BLOCK_DIST_PATH', FIGMA_BLOCK_PATH . 'dist/' );
+
+/**
+ * Get the minimum version of PHP required by this plugin.
+ *
+ * @return string Minimum version required.
+ */
+function minimum_php_requirement() {
+	return '7.4';
+}
+
+/**
+ * Whether PHP installation meets the minimum requirements
+ *
+ * @return bool True if meets minimum requirements, false otherwise.
+ */
+function site_meets_php_requirements() {
+	return version_compare( phpversion(), minimum_php_requirement(), '>=' );
+}
+
+// Ensuring our PHP version requirement is met first before loading plugin.
+if ( ! site_meets_php_requirements() ) {
+	add_action(
+		'admin_notices',
+		function () {
+			?>
+			<div class="notice notice-error">
+				<p>
+					<?php
+					echo wp_kses_post(
+						sprintf(
+							/* translators: %s: Minimum required PHP version */
+							__( 'The Figma Block plugin requires PHP version %s or later. Please upgrade PHP or disable the plugin.', 'figma-block' ),
+							esc_html( minimum_php_requirement() )
+						)
+					);
+					?>
+				</p>
+			</div>
+			<?php
+		}
+	);
+	return;
+}
 
 // Require Composer autoloader if it exists.
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
@@ -33,54 +76,10 @@ $is_local_env = in_array( wp_get_environment_type(), [ 'local', 'development' ],
 $is_local_url = strpos( home_url(), '.test' ) || strpos( home_url(), '.local' );
 $is_local     = $is_local_env || $is_local_url;
 
-if ( $is_local && file_exists( __DIR__ . '/dist/fast-refresh.php' ) ) {
+if ( $is_local && file_exists( __DIR__ . '/dist/fast-refresh.php' ) && function_exists( '\TenUpToolkit\set_dist_url_path' ) ) {
 	require_once __DIR__ . '/dist/fast-refresh.php';
-	TenUpToolkit\set_dist_url_path( basename( __DIR__ ), FIGMA_BLOCK_DIST_URL, FIGMA_BLOCK_DIST_PATH );
+	set_dist_url_path( basename( __DIR__ ), FIGMA_BLOCK_DIST_URL, FIGMA_BLOCK_DIST_PATH );
 }
 
-if ( ! function_exists( 'glob_recursive' ) ) {
-	/**
-	 * Recursively glob for a file
-	 *
-	 * @param string $pattern Pattern to glob
-	 * @param int    $flags Glob flags
-	 * @return array
-	 */
-	function glob_recursive( $pattern, $flags = 0 ) {
-		$files = glob( $pattern, $flags );
-
-		foreach ( glob( dirname( $pattern ) . '/*', GLOB_ONLYDIR | GLOB_NOSORT ) as $dir ) {
-			$files = array_merge( $files, glob_recursive( $dir . '/' . basename( $pattern ), $flags ) );
-		}
-
-		return $files;
-	}
-}
-
-/**
- * Register blocks
- */
-function register_blocks() {
-	$blocks_dist_path = FIGMA_BLOCK_PATH . 'dist/blocks/';
-
-	if ( file_exists( $blocks_dist_path ) ) {
-		$block_json_files = glob_recursive( $blocks_dist_path . '**/*/block.json' );
-
-		foreach ( $block_json_files as $filename ) {
-			$block_folder = dirname( $filename );
-			register_block_type( $block_folder );
-		}
-	}
-}
-add_action( 'init', __NAMESPACE__ . '\register_blocks' );
-
-/**
- * Load src modules/blocks
- */
-$src_modules = glob( FIGMA_BLOCK_PATH . 'src/*/index.php' );
-
-if ( ! empty( $src_modules ) ) {
-	foreach ( $src_modules as $src_path ) {
-		require_once $src_path;
-	}
-}
+$plugin = Plugin::get_instance();
+$plugin->enable();
