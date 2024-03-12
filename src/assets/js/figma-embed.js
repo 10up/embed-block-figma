4f3e3c95d6392b10be59318a76d4e0b22cf15760
Darin Kotter <darin.kotter@gmail.com>
2024-03-12 10:47:29 -0600

    Fix JS linting

--- src/assets/js/figma-embed.js
+++ src/assets/js/figma-embed.js
@@ -6,18 +6,15 @@ import { registerBlockVariation } from '@wordpress/blocks';
 
 import { Icon } from './icon';
 
-registerBlockVariation(
-	'core/embed',
-	{
-		name: 'figma',
-		title: 'Figma',
-		description: __('Embed Figma designs.'),
-		icon: Icon,
-		patterns: [ /https:\/\/[\w\.-]+\.?figma.com\/([\w-]+)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/i ],
-		attributes: {
-			providerNameSlug: 'figma',
-			responsive: true
-		},
-		isActive: (attributes) => attributes.providerNameSlug === 'figma',
-	}
-);
+registerBlockVariation('core/embed', {
+	name: 'figma',
+	title: 'Figma',
+	description: __('Embed Figma designs.'),
+	icon: Icon,
+	patterns: [/https:\/\/[\w.-]+\.?figma.com\/([\w-]+)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/i],
+	attributes: {
+		providerNameSlug: 'figma',
+		responsive: true,
+	},
+	isActive: (attributes) => attributes.providerNameSlug === 'figma',
+});
