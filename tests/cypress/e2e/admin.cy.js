24eb11b3e94dd14b2473f04c0a0b96abbaeb9240
sksaju <sksaaju@gmail.com>
2024-09-23 15:05:14 +0600

    update tests/cypress/e2e/admin.cy.js

--- tests/cypress/e2e/admin.cy.js
+++ tests/cypress/e2e/admin.cy.js
@@ -5,11 +5,6 @@ describe('Admin can login and make sure plugin is activated', () => {
 		cy.login();
 	});
 
-	it('Open dashboard', () => {
-		cy.visit('/wp-admin');
-		cy.get('h1').should('contain', 'Dashboard');
-	});
-
 	it('Can activate plugin if it is deactivated', () => {
 		cy.activatePlugin('embed-block-figma');
 	});
