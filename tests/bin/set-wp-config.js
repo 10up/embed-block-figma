7949c3509062c645cc9d4b4182bf6303e226169f
sksaju <sksaaju@gmail.com>
2024-11-07 03:05:15 +0600

    update set-wp-config.js

--- tests/bin/set-wp-config.js
+++ tests/bin/set-wp-config.js
@@ -7,27 +7,25 @@ const path = `${process.cwd()}/.wp-env.json`;
 let config = fs.existsSync(path) ? require(path) : { plugins: ['.', 'https://downloads.wordpress.org/plugin/classic-editor.zip'] };
 
 const args = {};
-process.argv
-	.slice(2, process.argv.length)
-	.forEach(arg => {
-		if (arg.slice(0, 2) === '--') {
-			const param = arg.split('=');
-			const paramName = param[0].slice(2, param[0].length);
-			const paramValue = param.length > 1 ? param[1] : true;
-			args[paramName] = paramValue;
-		}
-	});
+process.argv.slice(2, process.argv.length).forEach((arg) => {
+	if (arg.slice(0, 2) === '--') {
+		const param = arg.split('=');
+		const paramName = param[0].slice(2, param[0].length);
+		const paramValue = param.length > 1 ? param[1] : true;
+		args[paramName] = paramValue;
+	}
+});
 
 if (!args.core && !args.plugins) {
-	return;
+	process.exit(); // Exit instead of return
 }
 
-if ('latest' === args.core) {
+if (args.core === 'latest') {
 	delete args.core;
 }
 
 if (Object.keys(args).length === 0) {
-	return;
+	process.exit(); // Exit instead of return
 }
 
 if (args.plugins) {
@@ -37,7 +35,7 @@ if (args.plugins) {
 config = {
 	...config,
 	...args,
-}
+};
 
 try {
 	fs.writeFileSync(path, JSON.stringify(config));
