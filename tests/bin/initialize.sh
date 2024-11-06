50ee0ea70abf329ea82f8078457389356c9f97c5
sksaju <sksaaju@gmail.com>
2024-11-07 03:41:22 +0600

    update .wp-env.json and tests/bin/initialize.sh

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 wp-env run tests-wordpress chmod -c ugo+w /var/www/html
 wp-env run tests-cli wp rewrite structure '/%postname%/' --hard
-wp-env run tests-cli wp plugin install wp-mail-catcher --activate
-wp-env run tests-cli wp user get testuser || \
-wp-env run tests-cli wp user create testuser testuser@example.com --role=administrator
