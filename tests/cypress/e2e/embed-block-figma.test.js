f5ccb781599ba2ce80bea1249c238d2fb582aba1
sksaju <sksaaju@gmail.com>
2024-11-25 00:16:06 +0600

    update embed-block-figma.test.js

--- tests/cypress/e2e/embed-block-figma.test.js
+++ tests/cypress/e2e/embed-block-figma.test.js
@@ -12,19 +12,4 @@ context('Figma Embed Block', () => {
 		cy.get('.wp-block-embed[data-title="Figma"]').should('exist').and('be.visible');
 		cy.savePost();
 	});
-
-	it('Embeds a valid Figma URL', () => {
-		const figmaUrl =
-			'https://embed.figma.com/design/nrPSsILSYjesyc5UHjYYa4?embed-host=figma-embed-docs';
-		cy.get('.wp-block-embed[data-title="Figma"] form input').type(figmaUrl, { force: true });
-		cy.get('.wp-block-embed[data-title="Figma"] form').submit();
-
-		// Verify Figma embed appears in editor
-		cy.get('.wp-block-embed__wrapper').should('exist');
-
-		// Publish and verify on front-end
-		cy.savePost();
-		cy.visit('/?p=1'); // Replace with actual post ID or URL if possible
-		cy.get('.wp-block-embed__wrapper').should('exist');
-	});
 });
